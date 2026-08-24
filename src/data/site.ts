a79da3b60b78ef82cc58ecc69dd3e45acd42b6ad
startingmypersonalbrand-bit <startingmypersonalbrand@gmail.com>
2026-08-24 21:24:32 +0100

    Update site.ts

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,7 +4,6 @@ import authorSofia from "@/assets/author-sofia.jpg";
 import authorArjun from "@/assets/author-arjun.jpg";
 import authorClaire from "@/assets/author-claire.jpg";
 import authorDiego from "@/assets/author-diego.jpg";
-import raybartlett from "@/assets/ray-bartlett.jpg";
 
 export interface Author {
   id: string;
